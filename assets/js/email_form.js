2ad99aea9cbb3eae48a8d66c752ecbe7b964c383
Bart Stachurski <bart.stachurski@gmail.com>
2021-11-23 23:00:54 -0500

    Use contact_me for email contact form

---
+++ assets/js/email_form.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const signupForm = document.getElementById("contact-form")
+  signupForm.addEventListener("submit", function(event) {
+    event.preventDefault()
+    let data = {
+      subject: `${event.currentTarget.name.value} Sent You A Message From Github`,
+      from: event.currentTarget.email.value, 
+      body: event.currentTarget.message.value,
+      source: "Github.io",
+      to: "bartstachurski@hey.com"
+    }
+    fetch("https://sheltered-reaches-40837.herokuapp.com/api/emails", {
+      method: "POST",
+      body: JSON.stringify(data),
+      origin: "https://bartstachurski.github.io/",
+      headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+      .then(response => response.json()) 
+      .then(json => console.log(json))
+      .catch(err => console.log(err))
+  });
+});
